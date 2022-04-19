6d87ff5e57ba28c5c271fa15714f571c95f4cc60
Raul Talledo <raul.talledo@soapros.pe>
2022-04-18 20:44:22 -0500

    fix: correccion relase

--- dist/cognitoAuth.d.ts
+++ dist/cognitoAuth.d.ts
@@ -2,10 +2,42 @@ import { Handler } from 'express';
 export declare class CognitoAuth {
     private static dynamo;
     private static poolsDictionary;
+    /**
+     * Método principal para realizar la validación de los headers: Authorization y client_nexux
+     * @param req Request
+     * @param res Response
+     * @param next Siguiente función a procesar de pasar la validación
+     */
     static process: Handler;
+    /**
+     * Método para buscar en la tabla Cliente, la información del Pool y ClientId de Cognito para un determinado cliente
+     * @param id_client Id del cliente
+     * @param transacion_id Id de la transacción
+     */
     private static getDataClient;
+    /**
+     * Método que inicializa la descarga de la Firma pública para el cliente solicitado
+     * @param id_client Id del cliente
+     * @param transacion_id Id de la transaccion
+     * @returns
+     */
     private static init;
+    /**
+     * Validar el token y añadir la información del usuario en el request si la validación es exitosa
+     * @param pem Llave publica
+     * @param req Request
+     * @param res Response
+     * @param next Siguiente Función a procesar
+     */
     private static verifyMiddleWare;
+    /**
+     * Método para validar el token
+     * @param pems Llave pública
+     * @param auth Token de Authorization
+     * @param id_client ID del cliente
+     * @param transacion_id ID de la transacción
+     * @returns
+     */
     private static verify;
 }
 //# sourceMappingURL=cognitoAuth.d.ts.map
--- dist/cognitoAuth.js
+++ dist/cognitoAuth.js
@@ -36,21 +36,35 @@ exports.CognitoAuth = CognitoAuth;
 _a = CognitoAuth;
 CognitoAuth.dynamo = new aws_sdk_1.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: REGION });
 CognitoAuth.poolsDictionary = {};
+/**
+ * Método principal para realizar la validación de los headers: Authorization y client_nexux
+ * @param req Request
+ * @param res Response
+ * @param next Siguiente función a procesar de pasar la validación
+ */
 CognitoAuth.process = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        nexuxlog_1.Logger.message(nexuxlog_1.Level.info, { action: "validacion", id: req.id }, req.id.toString(), "validacion del header");
         //obtener el valor del header client_nexux
         let id_client = req.get(HEADER_CLIENT) || 'soapros';
-        nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, req.body, "req.body.id.toString()", "ingreso a la validacion");
-        const pemsDownloadProm = yield CognitoAuth.init(id_client);
-        nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, pemsDownloadProm, "req.body.id.toString()", "Llave publica");
+        nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, req.body, req.id.toString(), "ingreso a la validacion");
+        const pemsDownloadProm = yield CognitoAuth.init(id_client, req.id.toString());
+        nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, pemsDownloadProm, req.body.id.toString(), "Llave publica");
         //verificación usando el archivo JWKS
         CognitoAuth.verifyMiddleWare(pemsDownloadProm, req, res, next);
     }
     catch (err) {
-        console.error(err);
+        if (err instanceof Error) {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, {}, req.id.toString(), err.message);
+        }
     }
 });
-CognitoAuth.getDataClient = (id_client) => __awaiter(void 0, void 0, void 0, function* () {
+/**
+ * Método para buscar en la tabla Cliente, la información del Pool y ClientId de Cognito para un determinado cliente
+ * @param id_client Id del cliente
+ * @param transacion_id Id de la transacción
+ */
+CognitoAuth.getDataClient = (id_client, transacion_id) => __awaiter(void 0, void 0, void 0, function* () {
     var _b, _c, _d;
     let params = {
         TableName: TABLE_CLIENT,
@@ -59,7 +73,7 @@ CognitoAuth.getDataClient = (id_client) => __awaiter(void 0, void 0, void 0, fun
         },
         ProjectionExpression: "id, aws_cognito_clientapp_id, aws_cognito_userpool_id"
     };
-    nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, params, "id_client", "parametros de busqueda en la tabla cliente");
+    nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, params, transacion_id, "parametros de busqueda en la tabla cliente");
     let cognito = {
         id: "0",
         client_id: "0",
@@ -72,36 +86,48 @@ CognitoAuth.getDataClient = (id_client) => __awaiter(void 0, void 0, void 0, fun
             cognito.id = (_b = result.Item) === null || _b === void 0 ? void 0 : _b.id;
             cognito.client_id = (_c = result.Item) === null || _c === void 0 ? void 0 : _c.aws_cognito_clientapp_id;
             cognito.user_pool = (_d = result.Item) === null || _d === void 0 ? void 0 : _d.aws_cognito_userpool_id;
-            nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, result, "id_client", "resultado en la tabla cliente");
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, result, transacion_id, "resultado en la tabla cliente");
             CognitoAuth.poolsDictionary[id_client] = cognito;
         }
     }
     catch (e) {
         if (e instanceof Error) {
-            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, e, "id_client", "Error en la busqueda de la BD");
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, e, transacion_id, "Error en la busqueda de la BD");
             throw new Error(e.message);
         }
     }
 });
-CognitoAuth.init = (id_client) => {
+/**
+ * Método que inicializa la descarga de la Firma pública para el cliente solicitado
+ * @param id_client Id del cliente
+ * @param transacion_id Id de la transaccion
+ * @returns
+ */
+CognitoAuth.init = (id_client, transacion_id) => {
     return new Promise((resolve, reject) => {
+        nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { id_client }, transacion_id, "Descarga de la firma publica");
         let existSign = fs_1.default.existsSync(`/usr/${id_client}.pem`);
         if (!existSign) {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { id_client }, transacion_id, "Primera descarga de la firma publica");
             //cargar la data del tabla cliente
-            CognitoAuth.getDataClient(id_client)
+            CognitoAuth.getDataClient(id_client, transacion_id)
                 .then((result) => {
+                nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { result }, transacion_id, "se obtuvo la información del id_client");
                 //ruta de donde bajar la firma publica
                 let ISSUER = `https://cognito-idp.${REGION}.amazonaws.com/${CognitoAuth.poolsDictionary[id_client].user_pool}`;
                 const options = {
                     url: `${ISSUER}/.well-known/jwks.json`,
                     json: true
                 };
+                nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { options }, transacion_id, "Link de descarga");
                 //descargar la firma publica JWKS
                 request_1.default.get(options, (err, resp, body) => {
                     if (err) {
+                        nexuxlog_1.Logger.message(nexuxlog_1.Level.error, {}, transacion_id, err);
                         return reject(new Error("No se pudo descargar el JWKS"));
                     }
                     if (!body || !body.keys) {
+                        nexuxlog_1.Logger.message(nexuxlog_1.Level.error, {}, transacion_id, "Formato de JWSK");
                         return reject(new Error("Formato de JWSK no es el adecuado"));
                     }
                     const pems = {};
@@ -109,19 +135,30 @@ CognitoAuth.init = (id_client) => {
                         pems[key.kid] = (0, jwk_to_pem_1.default)(key);
                     }
                     fs_1.default.writeFileSync(`/usr/${id_client}.pem`, JSON.stringify(pems));
+                    nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { file: `/usr/${id_client}.pem` }, transacion_id, "Firma publica guardada");
                     resolve(pems);
                 });
             });
         }
         else { //leer la firma publica
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { file: `/usr/${id_client}.pem` }, transacion_id, "Firma publica leída");
             let sign = JSON.parse(fs_1.default.readFileSync(`/usr/${id_client}.pem`, "utf-8"));
             resolve(sign);
         }
     });
 };
+/**
+ * Validar el token y añadir la información del usuario en el request si la validación es exitosa
+ * @param pem Llave publica
+ * @param req Request
+ * @param res Response
+ * @param next Siguiente Función a procesar
+ */
 CognitoAuth.verifyMiddleWare = (pem, req, res, next) => {
-    CognitoAuth.verify(pem, req.get(HEADER_AUTHORIZATION), req.get(HEADER_CLIENT))
+    nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { Auth: req.get(HEADER_AUTHORIZATION), client: req.get(HEADER_CLIENT) }, req.id.toString(), "Función verifyMiddleWare");
+    CognitoAuth.verify(pem, req.get(HEADER_AUTHORIZATION), req.get(HEADER_CLIENT), req.id.toString())
         .then((decoded) => {
+        nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { Auth: req.get(HEADER_AUTHORIZATION), client: req.get(HEADER_CLIENT) }, req.id.toString(), "Verificación del token");
         if (typeof decoded !== "string") {
             //Asignar al Request información del usuario autenticado
             req.user = {
@@ -138,23 +175,37 @@ CognitoAuth.verifyMiddleWare = (pem, req, res, next) => {
                 req.user.email = decoded.email;
                 req.user.username = decoded['cognito:username'];
             }
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.info, { user: req.user }, req.id.toString(), "Informacion del usuario");
         }
-        console.log(`request: ${JSON.stringify(req.user)}`);
         next();
     }).catch((err) => {
-        const status = (err instanceof AuthError ? 401 : 500);
-        res.status(status).send(err.message || err);
+        if (err instanceof Error) {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, {}, req.id.toString(), err.message);
+            const status = (err instanceof AuthError ? 401 : 500);
+            res.status(status).send(err.message || err);
+        }
     });
 };
-CognitoAuth.verify = (pems, auth, id_client) => {
+/**
+ * Método para validar el token
+ * @param pems Llave pública
+ * @param auth Token de Authorization
+ * @param id_client ID del cliente
+ * @param transacion_id ID de la transacción
+ * @returns
+ */
+CognitoAuth.verify = (pems, auth, id_client, transacion_id) => {
     let ISSUER = `https://cognito-idp.${REGION}.amazonaws.com/${CognitoAuth.poolsDictionary[id_client].user_pool}`;
+    nexuxlog_1.Logger.message(nexuxlog_1.Level.debug, { ISSUER }, transacion_id, "verificación del token");
     return new Promise((resolve, reject) => {
         //verificar el formato del auth en el header
         if (!auth || auth.length < 10) {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "Formato no esperado, menor a 10 digitos");
             return reject(new AuthError("Invalido o ausente Authorization header. Esperado formato \'Bearer <your_JWT_token>\'. "));
         }
         const authPrefix = auth.substring(0, 7).toLowerCase();
         if (authPrefix !== 'bearer ') {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "El token no tiene el prefijo Bearer");
             return reject(new AuthError('Authorization header esperdo en el formato \'Bearer <your_JWT_token>\'.'));
         }
         //Obtener el token
@@ -163,19 +214,23 @@ CognitoAuth.verify = (pems, auth, id_client) => {
         const decodedNotVerified = jsonwebtoken_1.default.decode(token, { complete: true });
         //Verificar que exista el token decodificado
         if (!decodedNotVerified) {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "Authorization header contiene un token invalido");
             return reject(new AuthError('Authorization header contiene un token inválido'));
         }
         //Validar que la KID coincida con JWSK (Que el token haya sido firmado con la llave publica del USER_POOL)
         if (!decodedNotVerified.header.kid || !pems[decodedNotVerified.header.kid]) {
+            nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "el KID no coincide");
             return reject(new AuthError("Authorization header contiene un token inválido"));
         }
         //Decodificar la firma con la Llave publica
         jsonwebtoken_1.default.verify(token, pems[decodedNotVerified.header.kid], { issuer: ISSUER, maxAge: MAX_TOKEN_AGE }, (err, decodeAndVerified) => {
             if (err) {
                 if (err instanceof jsonwebtoken_1.default.TokenExpiredError) {
+                    nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "Authorzation header expirado");
                     return reject(new AuthError("Authorization header contiene un JWT que ha expirado en: " + err.expiredAt.toISOString()));
                 }
                 else {
+                    nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "JWT inválido");
                     return reject(new AuthError("Authorization header contiene un JWT inválido"));
                 }
             }
@@ -184,11 +239,13 @@ CognitoAuth.verify = (pems, auth, id_client) => {
             if (typeof decodeAndVerified !== "string") {
                 //validar que token_use = 'access'
                 if (ALLOWED_TOKEN_USES.indexOf(decodeAndVerified.token_use) === -1) {
+                    nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "Authorization contiene token inválido no ACCESS");
                     return reject(new AuthError('Authorization header contiene un token inválido.'));
                 }
                 //validar que client_id corresponda con el CLIENT_ID del USER_POOL
                 const clientId = (decodeAndVerified.aud || decodeAndVerified.client_id);
                 if (clientId !== CognitoAuth.poolsDictionary[id_client].client_id) {
+                    nexuxlog_1.Logger.message(nexuxlog_1.Level.error, { id_client, auth }, transacion_id, "Authozation contine token inválido CLIENT_ID no coincide");
                     return reject(new AuthError('Authorization header contiene un token inválido.')); // don't return detailed info to the caller
                 }
             }
